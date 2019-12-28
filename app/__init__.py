022124134d50ff5d4049e40b93b1fedc53db3591
William Kenrick <williamkenrick@Williams-MacBook-Pro-2.local>
2019-12-28 14:18:27 +0200

    Move errors to separate blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -25,8 +25,10 @@ bootstrap = Bootstrap(app)
 moment = Moment(app)
 babel = Babel(app)
 
+from app.errors import bp as errors_bp
+app.register_blueprint(errors_bp)
 
-from app import routes, models, errors
+from app import routes, models
 
 @babel.localeselector
 def get_locale():
---
+++ app/errors/__init__.py
@@ -0,0 +1,1 @@
+from app.errors.handlers import *
